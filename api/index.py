b3162b22abd4ecde7719d49528968818e38c7ebe
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-01-28 10:30:18 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,9 +3,9 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
+        self.send_response(302)
         self.send_header('Location','https://www.google.com/')
-        self.send_header('Content-type','text/plain')
+        #self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        #self.wfile.write('Hello, world!'.encode('utf-8'))
         return
