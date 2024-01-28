f2da44b07cb0462c2ff125c39c5cb461f0efdb14
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-01-28 16:53:31 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,30 +1,28 @@
 from http.server import BaseHTTPRequestHandler
 import json
 import os
-import requests
-from requests.auth import HTTPBasicAuth
+#import requests
+#from requests.auth import HTTPBasicAuth
 import random
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
-        token_value = os.environ['GITHUB_TOKEN']
+        #token_value = os.environ['GITHUB_TOKEN']
+	#plain_user = HTTPBasicAuth('', token_value)
 
-        plain_user = HTTPBasicAuth('', token_value)
+        mutuals = [ 'xiaoliu', 'mertaytore', 'yostx038' ]
 
-        mutuals = []
+        #payload = { "query": "query { user(login: \"lbonanomi\") { following(first:100) { nodes { login following(first: 100) { edges { node { login }}}}}}}" }
 
-        payload = { "query": "query { user(login: \"lbonanomi\") { following(first:100) { nodes { login following(first: 100) { edges { node { login }}}}}}}" }
+        #data=json.dumps(payload)
 
-        data=json.dumps(payload)
+        #graphql = requests.post("https://api.github.com/graphql", data=data, auth=plain_user)
 
-        graphql = requests.post("https://api.github.com/graphql", data=data, auth=plain_user)
-
-        for x in graphql.json()['data']['user']['following']['nodes']:
-	        for y in x['following']['edges']:
-		        if y['node']['login'] == 'lbonanomi':
-			        mutuals.append(x['login'])
+        #for x in graphql.json()['data']['user']['following']['nodes']:
+	#        for y in x['following']['edges']:
+	#	        if y['node']['login'] == 'lbonanomi':
+	#		        mutuals.append(x['login'])
 
         random_mutual = 'https://github.com/' + random.choice(mutuals)
         
