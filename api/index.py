a8be2e02639a492b76c903645efe215568e0675b
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2024-04-12 14:30:00 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -72,48 +72,11 @@ class handler(BaseHTTPRequestHandler):
       port=redis_config.port,
       username=redis_config.username, 
       password=redis_config.password,
+      decode_responses=True,
       ssl=True
     )
-    
-    token_value = os.environ['GITHUB_TOKEN']
 
-    headers = {"Content-type": "application/json", "Authorization": "bearer " + token_value, "User-Agent": "python3"}
-
-    conn = http.client.HTTPSConnection("api.github.com")
-    conn.request('GET', '/user', headers=headers)
-
-    try:
-      whoami = conn.getresponse().read().decode()
-      callme = json.loads(whoami)['login']
-    except KeyError:
-      # Author routinely runs out of GH tokens,
-      # so handle that
-      self.send_response(302)
-      self.send_header('Location', 'https://www.sonypictures.com/movies/thenet')
-      self.end_headers()
-      return
-          
-    payload = { "query": "query { user(login: \"" + callme + "\") { following(first:100) { nodes { login following(first: 100) { edges { node { login }}}}}}}" }
-
-    data = json.dumps(payload)
-
-    conn.request('POST', '/graphql', data, headers)
-
-    response = conn.getresponse()
-
-    a = response.read().decode()
-
-    mutuals = []
-
-    b = json.loads(a)
-
-    for x in b['data']['user']['following']['nodes']:
-      for y in x['following']['edges']:
-        if y['node']['login'] == callme:
-          mutuals.append(x['login'])
-
-
-    random_mutual = 'https://github.com/' + random.choice(mutuals)
+    random_mutual = r.srandmember("mutuals")
 
     self.send_response(302)
     self.send_header('Location',random_mutual)
